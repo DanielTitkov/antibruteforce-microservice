9b191cfa6bc976e16a01777ab2e7617192b2d792
DanielTitkov <mytasmic@gmail.com>
2019-11-29 12:41:25 +0300

    added concurrency support for bucket storage

--- internal/bucketstorage/bucketstorage.go
+++ internal/bucketstorage/bucketstorage.go
@@ -3,11 +3,14 @@ package bucketstorage
 import (
 	"context"
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/DanielTitkov/antibruteforce-microservice/internal/bucket"
 )
 
 type BucketStorage struct {
+	mx      sync.RWMutex
 	rubrics []string
 	M       map[string]map[string]bucket.Bucket
 }
@@ -18,12 +21,16 @@ type BucketArgs struct {
 	timespan int // seconds
 }
 
-func New(rubrics []string) (*BucketStorage, error) {
+func New(rubrics []string, clean int) (*BucketStorage, error) {
 	m := make(map[string]map[string]bucket.Bucket)
 	for _, s := range rubrics {
 		m[s] = make(map[string]bucket.Bucket)
 	}
-	return &BucketStorage{rubrics: rubrics, M: m}, nil
+	bs := BucketStorage{rubrics: rubrics, M: m}
+	if clean > 0 {
+		bs.runClean(clean)
+	}
+	return &bs, nil
 }
 
 func (bs *BucketStorage) Resolve(rubric, arg string, ba BucketArgs) (bool, error) {
@@ -31,16 +38,39 @@ func (bs *BucketStorage) Resolve(rubric, arg string, ba BucketArgs) (bool, error
 	if !ok {
 		return false, fmt.Errorf("Rubric is not present: %s", rubric)
 	}
+	bs.mx.Lock()
 	b, ok := m[arg]
-	if !ok {
+	if !ok || !b.IsAlive() {
 		b, err := bucket.New(ba.ctx, ba.rate, ba.timespan)
 		if err != nil {
 			return false, err
 		}
 		m[arg] = b
 	}
-	// TODO: check if bucket is alive!
 	b, _ = m[arg]
 	res := b.Add()
+	bs.mx.Unlock()
 	return res, nil
+}
+
+func (bs *BucketStorage) Clean() error {
+	bs.mx.Lock()
+	for _, v := range bs.M {
+		for ki, vi := range v {
+			if !vi.IsAlive() {
+				delete(v, ki)
+			}
+		}
+	}
+	bs.mx.Unlock()
+	return nil
+}
+
+func (bs *BucketStorage) runClean(sleepMS int) {
+	go func() {
+		for {
+			bs.Clean()
+			time.Sleep(time.Millisecond * time.Duration(sleepMS))
+		}
+	}()
 }
--- internal/bucketstorage/bucketstorage_test.go
+++ internal/bucketstorage/bucketstorage_test.go
@@ -2,13 +2,15 @@ package bucketstorage
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestOverfillInStorage(t *testing.T) {
 	rubrics := []string{"ip", "login"}
-	bs, err := New(rubrics)
+	bs, err := New(rubrics, 0)
 	if err != nil {
 		t.Errorf("storage creation failed: %v", err)
 	}
@@ -31,7 +33,7 @@ func TestOverfillInStorage(t *testing.T) {
 
 func TestLeakingResultInMultirubricStorage(t *testing.T) {
 	rubrics := []string{"ip", "login"}
-	bs, err := New(rubrics)
+	bs, err := New(rubrics, 0)
 	if err != nil {
 		t.Errorf("storage creation failed: %v", err)
 	}
@@ -52,6 +54,111 @@ func TestLeakingResultInMultirubricStorage(t *testing.T) {
 				}
 			}
 		}
+		time.Sleep(time.Second) // wait till buckets' queues leak out
+	}
+}
+
+func TestDeadBucketManualClean(t *testing.T) {
+	rubrics := []string{"ip"}
+	bs, err := New(rubrics, 0)
+	if err != nil {
+		t.Errorf("storage creation failed: %v", err)
+	}
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	ba := BucketArgs{ctx, 100, 1}
+	bs.Resolve("ip", "foo", ba)
+	if _, ok := bs.M["ip"]["foo"]; !ok {
+		t.Errorf("expected bucket to exist after resolve, got %v", ok)
+	}
+	cancel()
+	err = bs.Clean()
+	if err != nil {
+		t.Errorf("error occured during clean")
+	}
+	if _, ok := bs.M["ip"]["foo"]; ok {
+		t.Errorf("expected bucket to be absent after clean, got %v", ok)
+	}
+}
+
+func TestDeadBucketAutoClean(t *testing.T) {
+	rubrics := []string{"ip"}
+	bs, err := New(rubrics, 10)
+	if err != nil {
+		t.Errorf("storage creation failed: %v", err)
+	}
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	ba := BucketArgs{ctx, 100, 1}
+	bs.Resolve("ip", "foo", ba)
+	if _, ok := bs.M["ip"]["foo"]; !ok {
+		t.Errorf("expected bucket to exist after resolve, got %v", ok)
+	}
+	cancel()
+	time.Sleep(time.Millisecond * 20)
+	if _, ok := bs.M["ip"]["foo"]; ok {
+		t.Errorf("expected bucket to be absent after clean, got %v", ok)
+	}
+}
+
+func TestOverfillWithConcurrency(t *testing.T) {
+	rubrics := []string{"ip", "login"}
+	bs, err := New(rubrics, 0)
+	if err != nil {
+		t.Errorf("storage creation failed: %v", err)
+	}
+	var passed uint64
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		ctx := context.Background()
+		ba := BucketArgs{ctx, 10, 60}
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			res, err := bs.Resolve("ip", "123.123.123.123", ba)
+			if err != nil {
+				t.Errorf("error occured during resolving at iteration %d: %v", i, err)
+			}
+			if res {
+				atomic.AddUint64(&passed, 1)
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	if passed != 10 && passed != 11 { // 10 or 11 because one of elements may leak before all routines do their stuff
+		t.Errorf("expected 10 or 11, got %d", passed)
+	}
+}
+
+func TestLeakingWithConcurrency(t *testing.T) {
+	rubrics := []string{"ip", "login"}
+	bs, err := New(rubrics, 0)
+	if err != nil {
+		t.Errorf("storage creation failed: %v", err)
+	}
+	for j := 0; j < 2; j++ { // two times
+		var wg sync.WaitGroup
+		for i := 0; i < 90; i++ {
+			ctx := context.Background()
+			ba := BucketArgs{ctx, 100, 1}
+			for _, r := range rubrics {
+				for _, a := range []string{"123.123", "321.321", "999.999"} {
+					wg.Add(1)
+					go func(r, a string) {
+						defer wg.Done()
+						res, err := bs.Resolve(r, a, ba)
+						if err != nil {
+							t.Errorf("error occured during resolving at iteration %d-%d rub %s arg %s: %v", i, j, r, a, err)
+						}
+						if res != true {
+							t.Errorf("expeted true, got %v on iteration %d-%d rub %s arg %s", res, i, j, r, a)
+						}
+					}(r, a)
+				}
+			}
+		}
+		wg.Wait()
 		time.Sleep(time.Second) // wait till buckets' queues leak out
 	}
 }
